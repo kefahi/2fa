bd19e2797ee16f175b65bcd9ca420384e1868fe9
Kefah Issa <kefah.issa@gmail.com>
2017-11-30 01:57:37 +0300

    Improved error handling

--- 2fa.py
+++ 2fa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import hmac, base64, struct, hashlib, time
 
-def otp(secret):
+def totp(secret):
     secret = secret.replace(" ", "")
     secret += '=' * (-len(secret) % 8)  # Add correct '=' padding
     intervals_no = int(time.time())//30 
@@ -15,29 +15,30 @@ def otp(secret):
 import json,sys,argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-j", "--json", help="The 2fa json file", default="./2fa.json")
+parser.add_argument("-j", "--json", help="The 2fa json file", default="./my2fa.json")
 parser.add_argument("-m", "--match", help="Only match entries that contain the provided string", default="")
 parser.add_argument("-w", "--wait", help="Wait until the OTP expires and show count-down", action="store_true")
 args = parser.parse_args()
 
 try:
     myfile = open(args.json, 'rb')
-except IOError:
-    print("Failed to open the json file")
+    raw = myfile.read()
+    myfile.close()
+    data = json.loads( raw )
+    for one in data['Accounts']: # Validate against the expected schema and format.
+        if not isinstance(one['Name'],str) or not isinstance(one['Secret'],str) or not base64.b32decode(one['Secret']+'=' * (-len(one['Secret']) % 8),True):
+            raise Exception("The json schema doesn't comply")
+except Exception as err:
+    print("Failed to process the json file:", err)
     sys.exit(1)
 
-data = json.loads( myfile.read() )
-accounts = data['Accounts']
-myfile.close()
-
 expires_at = int(time.time()+30)//30*30
-for item in accounts:
+for item in data['Accounts']:
     if not args.match or args.match.lower() in item['Name'].lower(): 
-        print("%+12s -- %06d" % (item['Name'], otp(item['Secret'])))
+        print("%+12s -- %06d" % (item['Name'], totp(item['Secret'])))
 
 print()
 
-# Calc remaining
 remaining_seconds = (expires_at - time.time())
 
 if args.wait:
